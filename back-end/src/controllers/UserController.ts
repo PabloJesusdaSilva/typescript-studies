e059cba71870755bdd76a9130ed596790ed794aa
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-10-19 16:50:20 -0200

    :tada: initial commit

---
+++ back-end/src/controllers/UserController.ts
@@ -0,0 +1,33 @@
+import { Request, Response} from 'express'
+
+import EmailService from '../services/EmailService';
+
+const users = [
+   {name: "Pablo", email: "pablosilva@gmail.com"}
+]
+
+export default {
+   async index(req: Request, res: Response) {
+      return res.json(users);
+   },
+
+   async create(req: Request, res: Response) {
+      const emailService = new EmailService();
+
+      emailService.sendMail(
+         {
+            to: {
+               name: 'Pablo', 
+               email: 'pablo@gmail.com'
+            },
+            message: {
+               subject: 'Bem vindo ao sistema', 
+               body: 'Seja bem vindo'
+            }
+         }
+      )
+
+      return res.send();
+   }
+
+};
